a0a9eb559d3ba12fac995ee68f0af82a8ca4e333
cornelialiu <cornelia.t.liu@gmail.com>
2016-03-13 06:06:16 -0400

    add src for init Torbird

---
+++ TorBird/src/html/src/com/torontobird/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.torontobird.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.torontobird.TBGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TBGame();
+        }
+}
