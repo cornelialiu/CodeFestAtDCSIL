651e53361bec5f2bb2582e7efd05f3ab0fbd50b0
cornelialiu <cornelia.t.liu@gmail.com>
2016-03-20 12:42:22 -0400

    add init of TorBird

---
+++ TorBird_init/src/desktop/src/com/torbird/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.torbird.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.torbird.game.TBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TBGame(), config);
+	}
+}
